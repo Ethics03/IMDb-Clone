a451105d09a2f979fdd203c40c51a0b3a1b87be0
Rachit Srivastava <honey032004@gmail.com>
2024-10-04 23:26:46 +0530

    addedInterface

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+interface Movie {
+    backdrop_path?: string | null; 
+    id: number;
+    title: string;
+    original_title?: string; // Optional property
+    overview?: string; // Optional property
+    poster_path?: string | null; 
+    media_type: 'movie' | 'tv'; 
+    adult: boolean;
+    original_language: string;
+    genre_ids: number[]; 
+    popularity: number;
+    release_date: string; 
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    origin_country?: string[]; // Optional property
+  }
+
+  type res = Movie[]
 
 
-
-function Results({results}:{results:any}) {
+function Results({results}:{results:res}) {
   return(
 
   <>
