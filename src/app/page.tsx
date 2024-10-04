8cfae5ab345fa4096b15c17c41bd33957da80162
Rachit Srivastava <honey032004@gmail.com>
2024-10-04 22:29:37 +0530

    calledAPI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Results from '../Components/Results'
 
-function Home() {
+
+const API_KEY = process.env.API_KEY;
+interface SearchParams{
+  genre: string
+}
+
+
+
+
+ export default async function Home({searchParams}:{searchParams:SearchParams}) {
+
+  const genre = searchParams.genre || 'fetchTrending';
+
+    const res = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
+        language=en-US&page=1`
+      
+    );
+    
+    const data = await res.json();
+    if(!res.ok){
+      throw new Error('Failed to Fetch');
+    }
+    else{
+      const results:Results = data.results;
+      console.log(results)
+    }
+  
   return (
+  <div>
     <div>Home</div>
+   </div>
   )
 }
 
-export default Home
