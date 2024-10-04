bee813275e021fa31e778138656f63df83736c31
Rachit Srivastava <honey032004@gmail.com>
2024-10-05 00:04:14 +0530

    addedLoadingeffect

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function loading() {
+  return (
+    <div className = 'flex justify-center mt-16'>
+        <img className = 'h-52' src = 'load.svg' alt = 'loading'></img>
+    </div>
+  )
+}
+
+export default loading
