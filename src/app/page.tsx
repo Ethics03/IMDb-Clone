98adb3f728594ad2caba7f168bdd11a933e48632
Rachit Srivastava <honey032004@gmail.com>
2024-10-05 16:42:38 +0530

    TrendingAsDay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const inter = Inter({
 
     const res = await fetch(
       `https://api.themoviedb.org/3${
-        genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
+        genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/day`}?api_key=${API_KEY}&
         language=en-US&page=1`
       
     );
